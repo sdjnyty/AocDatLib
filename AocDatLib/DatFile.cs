f8c8a37d60bdb7d5c984761d3ee9dffa10308497
yty <sdjn-yty@163.com>
2017-10-31 01:37:55 +0800

    add ConsoleTest

---
+++ AocDatLib/DatFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace YTY.AocDatLib
+{
+  public class DatFile
+  {
+    private char[] _version;
+
+    public DatFile(string fileName)
+    {
+      using (var ds = new DeflateStream(new FileStream(fileName, FileMode.Open), CompressionMode.Decompress))
+      {
+        using (var ms = new MemoryStream())
+        {
+          ds.CopyTo(ms);
+          ms.Seek(0, SeekOrigin.Begin);
+
+          using (var br = new BinaryReader(ms, Encoding.ASCII))
+          {
+            _version = br.ReadChars(8);
+            var nTerrainRestrictions = br.ReadUInt16();
+            var nTerrains = br.ReadUInt16();
+            var pTerrainRestrictions0 = new int[nTerrainRestrictions];
+            var pTerrainRestrictions1 = new int[nTerrainRestrictions];
+            for (var i = 0; i < nTerrainRestrictions; i++)
+            {
+              pTerrainRestrictions0[i] = br.ReadInt32();
+            }
+            for (var i = 0; i < nTerrainRestrictions; i++)
+            {
+              pTerrainRestrictions1[i] = br.ReadInt32();
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTY.AocDatLib
+{
+  public static class Program
+  {
+    public static void Main(string[] args)
+    {
+      var dat = new DatFile(@"D:\Hawkaoc\aoc\Data\empires2.dat");
+      Console.ReadKey();
+    }
+  }
+}
